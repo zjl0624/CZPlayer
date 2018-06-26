500c79b8235337e5c50aa80d2e253d9ec448d82b
zjl <369907659@qq.com>
2018-06-26 13:30:45 +0800

    新建工程

---
+++ CustomAVPlayer/CustomAVPlayer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CustomAVPlayer
+//
+//  Created by zjl on 2018/6/26.
+//  Copyright © 2018年 zjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
